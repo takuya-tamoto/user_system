1f9fa9bb9b2a6cb3ffb4fb50499ee2389a3b94ea
tamoto <taku92860110@gmail.com>
2020-09-25 17:29:21 +0900

    ユーザー更新機能まで作成

---
+++ src/main/java/com/example/demo/login/acpect/LogAspect.java
@@ -0,0 +1,51 @@
+package com.example.demo.login.acpect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LogAspect {
+
+	@Around("@within(org.springframework.stereotype.Controller)")
+	public Object startLog(ProceedingJoinPoint jp) throws Throwable {
+		System.out.println("メソッド開始: " + jp.getSignature());
+
+		try {
+			Object result = jp.proceed();
+			System.out.println("メソッド終了: " + jp.getSignature());
+			return result;
+		} catch (Exception e) {
+			System.out.println("メソッド以上終了: " + jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
+	}
+
+	@Around("execution(* *..*.*UserDao*.*(..))")
+	public Object daoLog(ProceedingJoinPoint jp) throws Throwable {
+
+		System.out.println("メソッド開始： " + jp.getSignature());
+
+		try {
+
+			Object result = jp.proceed();
+
+			System.out.println("メソッド終了： " + jp.getSignature());
+
+			return result;
+
+		} catch (Exception e) {
+			System.out.println("メソッド異常終了： " + jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
+	}
+	//	@After("execution(* *..*.*Controller.*(..))")
+	//	public void sndLog(JoinPoint jp) {
+	//		System.out.println("メソッド終了: " + jp.getSignature());
+	//	}
+
+}
